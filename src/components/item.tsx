6548d13c8eb3c0e474fe65b798870129d49a1743
mikemajara <fmiguel05@gmail.com>
2021-06-22 14:54:56 +0200

    refactor: adjust styles

--- src/components/item.tsx
+++ src/components/item.tsx
@@ -29,7 +29,7 @@ import {
   AText,
 } from "./chakra-animated-components";
 
-const transition = { duration: .2 };
+const transition = { type: "spring", bounce: 0 };
 
 export function Item({ id }) {
   const { category, title } = items.find((item) => item.id === id);
@@ -37,19 +37,19 @@ export function Item({ id }) {
   const text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
   return (
     <>
-      <AnimateSharedLayout type="switch">
+      <AnimateSharedLayout type="crossfade">
         <ABox
           p="8"
           justify="start"
           onClick={onOpen}
           layoutId={`container-${id}`}
-          shadow="md"
+          shadow="lg"
           style={{
-            // border: "1px solid",
-            // borderColor: "gray",
+            border: "1px solid",
+            borderColor: "lightgrey",
             borderRadius: "10px",
-            width: "300px",
-            Height: "600px"
+            width: "400px",
+            height: "300px"
           }}
           position="relative"
           animate={transition}
@@ -62,8 +62,8 @@ export function Item({ id }) {
             layoutId={`heading-${id}`}
             style={{
               position: "absolute",
-              top: "15px",
-              left: "15px",
+              top: "20px",
+              left: "20px",
               maxWidth: "300px",
             }}
           >
@@ -97,19 +97,19 @@ export function Item({ id }) {
               >
               <Heading>Modal Title</Heading>
               </ABox>
-              <ABox mt={16} animate exit={{opacity: 0, ...transition}}>
+              <ABox py={16} animate exit={{opacity: 0, ...transition}}>
                 {text}
               </ABox>
             </AModalBody>
 
-            <AModalFooter animate exit={{opacity: 0, ...transition}}>
+            {/* <AModalFooter animate exit={{opacity: 0, ...transition}}>
               <Button colorScheme="blue" mr={3} onClick={onClose}>
                 Close
               </Button>
               <Button variant="ghost">
                 Secondary Action
               </Button>
-            </AModalFooter>
+            </AModalFooter> */}
           </AModalContent>
         </Modal>
       </AnimateSharedLayout>
